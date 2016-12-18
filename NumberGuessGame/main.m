11ccb9f6e1652f0a07dee12a163d0a57677e004e
Divya Iyengar <divyaiyengar@divyas-mbp>
2016-12-17 18:29:25 -0800

    Initial Commit

---
+++ NumberGuessGame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NumberGuessGame
+//
+//  Created by Divya Iyengar on 12/17/16.
+//  Copyright © 2016 Divya Iyengar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
